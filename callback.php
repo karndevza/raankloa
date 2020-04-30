d5ecf3a77ec8433cb516aec2367c0dab6d284d9a
karndevza <karnkung30@gmail.com>
2020-04-30 22:37:57 +0700

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,57 @@
+<?php 
+
+$API_URL = 'https://api.line.me/v2/bot/message';
+$ACCESS_TOKEN = 'NSHvuXtd3LdmsJ76r43DO7SHO6Bl06CWi16ZzY8IwmWKwmVMDOvSOgsYJz7A9JECk40lpbaia3a9fiMW3VWChzR9lMITMfcF9Dx64hyi6gvbAknKDfM2XazH74FeCsGclwdnYnaK7xAZTZF8Atci0wdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = 'dbbd09910b14b196d858df2db1374e8f';
+
+
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+
+
+if ( sizeof($request_array['events']) > 0 ) {
+
+    foreach ($request_array['events'] as $event) {
+
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+        
+    }
+}
+
+echo "OK";
+
+
+
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+
+
+?>
